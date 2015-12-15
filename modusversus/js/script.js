12db114c56368b0d9cfb8402c16114c11ee1742b
IngeGerda Lungu <IngaGerdaL@gmail.com>
2015-12-15 18:27:29 +0200

    first commit

---
+++ modusversus/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $('.bxslider').bxSlider({
+        pager: false,
+        speed: 2000
+    });
+    $('.slider').bxSlider({
+        minSlides: 3,
+        maxSlides: 6,
+        slideWidth: 101,
+        slideMargin: 80,
+        pager: false
+    });
+});
+
+
